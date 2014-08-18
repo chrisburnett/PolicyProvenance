228f10b5d4334989f9892f1bfd013d8cf798f419
Chris Burnett <dameramu@gmail.com>
2014-08-18 13:39:55 +0100

    Added powerset generation and inference filter

--- src/main/java/uk/ac/abdn/csd/polprov/Experiment.java
+++ src/main/java/uk/ac/abdn/csd/polprov/Experiment.java
@@ -1,28 +1,37 @@
 package uk.ac.abdn.csd.polprov;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.topbraid.spin.inference.SPINInferences;
 import org.topbraid.spin.system.SPINModuleRegistry;
 import org.topbraid.spin.util.JenaUtil;
 
+import com.google.common.collect.Sets;
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
 
 public class Experiment 
 {
+	
+	public static final String POLICY_NAMESPACE = "http://www.dotrural.ac.uk/ontologies/policies#";
+	
     public static void main( String[] args )
     {
     	
     	// the following is based largely on the examples in the SPIN API
-        System.out.println( "Running..." );
+        System.out.println( "Starting up..." );
         System.out.println(System.getProperty("user.dir"));
 
 		// Initialize system functions and templates
 		SPINModuleRegistry.get().init();
         
         // load in the policy, scenario and rule files
+		System.out.println("Loading models...");
         Model model = ModelFactory.createDefaultModel(); 
         model.read("file:rdf/hypertensionScenario.rdf");
         model.read("file:rdf/hypertensionPolicy.rdf");
@@ -41,20 +50,28 @@ public class Experiment
 		SPINModuleRegistry.get().registerAll(ontModel, null);
 
 		// Run all inferences
+		System.out.println("Running inference rules...");
 		SPINInferences.run(ontModel, policyTriples, null, null, false, null);
 		System.out.println("Inferred policy enactment triples: " + policyTriples.size());
         model.read("file:rdf/policies.rdf");
 		SPINInferences.run(ontModel, possibleTriples, null, null, false, null);
-		System.out.println("Possible PROV triples inferred: " + possibleTriples.size());
 
+		// Filter out pol inferences into a set
         StmtIterator st = possibleTriples.listStatements();
+        Set<Statement> inferenceSet = new HashSet<Statement>();
         while(st.hasNext()) {
-        	System.out.println(st.next().toString());
+        	Statement s = st.next();
+        	if(s.getPredicate().getNameSpace().equals(POLICY_NAMESPACE))
+        		inferenceSet.add(s);
         }
+		System.out.println("Possible PROV triples inferred: " + inferenceSet.size());
 
 
         
         // generate the power set of 'degradation situations'
+        System.out.print("Generating power set of provenance degradations...");
+        Set<Set<Statement>> pset = Sets.powerSet(inferenceSet);
+        System.out.println("finished " + pset.size() + " situations.");
         
         // for each one run the rules and see how many inferences we keep
         // for now, no breakdown by rule type
