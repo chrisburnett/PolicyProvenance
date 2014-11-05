3e0d045fc9b350f87de0a95efaa127521b6b21c0
Chris Burnett <dameramu@gmail.com>
2014-11-05 03:05:24 +0000

    Run script

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_67.jdk/Contents/Home
+#export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_67.jdk/Contents/Home
 mvn exec:java -Dexec.mainClass=uk.ac.abdn.csd.polprov.Experiment
